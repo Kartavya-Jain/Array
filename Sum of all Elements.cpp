5f3549987543f16257df47d6fb14b2c79f87d8c3
Kartavya Jain <99114718+Kartavya-Jain@users.noreply.github.com>
2025-08-17 18:07:41 +0000

    new file:   Sum of all Elements new file:   Sum of all Elements.cpp

---
+++ Sum of all Elements.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,sum=0;
+    cout<<"Enter the size of the array:";
+    cin>>n;
+    int* array=new int[n];
+    cout<<"Enter the elements of the array:";
+    for(int i=0;i<n;i++)
+    {
+        cin>>array[i];
+    }
+    cout<<array[0];
+    for(int i=1;i<n;i++)
+    {
+        cout<<" "<<array[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        sum=sum+array[i];
+    }
+    cout<<endl;
+    cout<<sum;
+   delete[] array;
+   array=nullptr;
+   return 0;
+}
